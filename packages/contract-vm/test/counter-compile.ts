d934a052f77e2798c92e7f84ed959d264b0dc4b7
Judd <kepzorz@gmail.com>
2019-03-19 17:27:06 -0700

    update test

--- packages/contract-vm/test/counter-compile.ts
+++ packages/contract-vm/test/counter-compile.ts
@@ -1,41 +1,47 @@
-let compile = require('contract-compiler')
 let { Contract, Host } = require('../')
 let { parse, stringify } = require('deterministic-json')
 
 async function main() {
   // Test single contract
-  let counterCode = await compile(`contracts/counter.ts`)
-  let countifierCode = await compile(`contracts/countifier.ts`)
-  let greeterCode = await compile(`contracts/greeter.ts`)
-
-  // Test multi-contract host
-  let host = new Host()
-  let counterAddress = host.addContract(counterCode)
-  console.log('counter address:')
-  console.log(counterAddress)
-  let countifierAddress = host.addContract(countifierCode)
-
-  let greeterAddress = host.addContract(greeterCode)
-  console.log('greeter address:')
-  console.log(greeterAddress)
-
-  let message = {
-    sender: 'judd',
-    to: counterAddress,
-    method: 'increment',
-    data: [3]
+  let contract = new Contract(`
+  module.exports = {
+    foo() {
+      console.log('called foo')
+      return 4
+    }
   }
+  `)
 
-  function consumeGas(gas: any) {
-    console.log(`consumed ${gas} gas`)
-  }
-  console.log(host.execute(message, consumeGas))
+  console.log(contract)
 
-  let counter = host.contracts[counterAddress]
-  let countifier = host.contracts[countifierAddress]
-  console.log(counter.contract.increment(20)) // 20
-  console.log(countifier.contract.addToCount(10)) // 30
-  console.log(counter.contract.count) // 30
-  console.log(host.contracts[greeterAddress].contract.greet('world'))
+  // // Test multi-contract host
+  // let host = new Host()
+  // let counterAddress = host.addContract(counterCode)
+  // console.log('counter address:')
+  // console.log(counterAddress)
+  // let countifierAddress = host.addContract(countifierCode)
+
+  // let greeterAddress = host.addContract(greeterCode)
+  // console.log('greeter address:')
+  // console.log(greeterAddress)
+
+  // let message = {
+  //   sender: 'judd',
+  //   to: counterAddress,
+  //   method: 'increment',
+  //   data: [3]
+  // }
+
+  // function consumeGas(gas: any) {
+  //   console.log(`consumed ${gas} gas`)
+  // }
+  // console.log(host.execute(message, consumeGas))
+
+  // let counter = host.contracts[counterAddress]
+  // let countifier = host.contracts[countifierAddress]
+  // console.log(counter.contract.increment(20)) // 20
+  // console.log(countifier.contract.addToCount(10)) // 30
+  // console.log(counter.contract.count) // 30
+  // console.log(host.contracts[greeterAddress].contract.greet('world'))
 }
 main()
