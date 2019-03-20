f2240d11187defa9ddd7eba493da0473e70869b4
Judd <kepzorz@gmail.com>
2019-03-19 21:06:53 -0700

    persist state between contract method calls

--- packages/contract-vm/src/contract.ts
+++ packages/contract-vm/src/contract.ts
@@ -1,33 +1,59 @@
 let metering = require('metering')
 let SES = require('ses')
+let { stringify, parse } = require('deterministic-json')
 
 export class Contract {
   public exports: any
 
+  public state: any
   private meteredCode: string
 
   constructor(
     public code: string,
-    private bindings = {},
-    private consumeGas?: any
+    private consumeGas?: any,
+    initialState: any = {}
   ) {
     // TODO: randomly generated metering function name to prevent shadowing
+    this.state = initialState
     let meteringFunctionName = 'consumeGas'
     this.meteredCode = metering('consumeGas', this.code)
     const rootRealm: any = SES.makeSESRootRealm({
       consoleMode: 'allow',
       errorStackMode: 'allow'
     })
-    this.exports = rootRealm.evaluate(this.meteredCode, {
-      [meteringFunctionName]: (value: any) => {
-        if (this.consumeGas) {
-          this.consumeGas()
+    this.exports = new Proxy(
+      rootRealm.evaluate(this.meteredCode, {
+        [meteringFunctionName]: (value: any) => {
+          if (this.consumeGas) {
+            this.consumeGas()
+          }
+          return value
+        },
+        JSON: {},
+        module: { exports: {} }
+      }),
+      {
+        get: (target, key) => {
+          if (typeof target[key] === 'function') {
+            return (...args: any[]) => {
+              this.initialize()
+              let result = target[key](...args)
+              // save state from contract
+              Object.assign(this.state, parse(stringify(this.exports)))
+              return result
+            }
+          } else {
+            return target[key]
+          }
         }
-        return value
-      },
-      JSON: {},
-      module: { exports: {} }
-    })
+      }
+    )
+
+    Object.assign(this.exports, initialState)
+  }
+
+  initialize() {
+    Object.assign(this.exports, this.state)
   }
 
   useMeter(consumeGas?: any) {
