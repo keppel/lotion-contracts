07ade2fe0f709729217e3011d7bd1411dedcc715
Judd <kepzorz@gmail.com>
2019-04-10 16:10:30 -0700

    test contract-vm

--- packages/contract-vm/test/counter-compile.js
+++ packages/contract-vm/test/counter-compile.js
@@ -1,8 +1,9 @@
 let { Contract, Host } = require('../')
 let { parse, stringify } = require('deterministic-json')
 let fs = require('fs')
+let test = require('tape')
 
-async function main() {
+test('saving and loading hosts', function(t) {
   // Test single contract
   let counterCode = fs.readFileSync('./test/contracts/counter.js').toString()
 
@@ -16,7 +17,7 @@ async function main() {
   // console.log('counter address: ' + counterAddress)
   let otherHost = new Host({})
   otherHost.load(saved)
-  console.log(host.contracts[counterAddress].exports.count) // 9
-  console.log(otherHost.contracts[counterAddress].exports.count) // 7
-}
-main()
+  t.equals(host.contracts[counterAddress].exports.count, 9) // 9
+  t.equals(otherHost.contracts[counterAddress].exports.count, 7) // 7
+  t.end()
+})
